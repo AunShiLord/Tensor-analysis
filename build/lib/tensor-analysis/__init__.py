8293215d49b8e1eb721397ad4e759412b58fe477
AunShiLord <aunshilord@yahoo.com>
2015-06-03 02:15:11 +0700

    Added setup files

---
+++ build/lib/tensor-analysis/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python27
+"""A module to manipulate symbolic objects with indices including tensors
+
+"""
+from .indexed import IndexedBase, Idx, Indexed
+from .index_methods import get_contraction_structure, get_indices
+from .arraypy import Arraypy, TensorArray, matrix2arraypy, matrix2tensor,\
+    list2arraypy, list2tensor
+from .tensor_fields import Wedge_array, df, grad, curl, diverg, lie_xy, dw, \
+    lie_w, tensor2wedgearray, wedgearray2tensor, int_product, g_tensor, \
+    g_wedge, hodge_star, codiff
+from .helper_functions import check_vector_of_arguments, check_metric_tensor, \
+    check_the_vector_field, sign_permutations, delete_index_from_list, \
+    replace_index_to_k
+from .riemannian_geometry import scal_prod, christoffel_1, christoffel_2,\
+    covar_der, covar_der_XY, riemann, ricci, scal_curv, k_sigma
+from .tensor_methods import symmetric, asymmetric, tensor_product, wedge,\
+    perm_parity, change_basis, lower_index, raise_index, is_symmetric,\
+    is_asymmetric
---
+++ setup.py
@@ -0,0 +1,46 @@
+# tensor-analysis setup.py
+# from distutils.core import setup
+from setuptools import setup
+setup(
+    name = "tensor-analysis",
+    packages = ["tensor-analysis"],
+    version = "0.9.0",   
+    description = "Tensor analysis package for n-dimensional tensor calculation",
+    author = "Vladimir Kuzmin, Konovalenko Anastasia, Merzlyakova Ksenia",
+    author_email = "aunshilord@yahoo.com",
+    url = "https://github.com/AunShiLord/Tensor-analysis.git",
+    keywords = ["tensor", "tensor_fields", "riemannian", "arraypy", "sympy",\
+                "tensor_algebra"],
+    install_requires = ["sympy"], 
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",       
+        ],
+    long_description = """\
+    Tensor analysis package.
+    This is a separate version of what_we_commited_to_sympy.
+    Consists of:
+    Arraypy class - N-dimentional arrays
+    TensorArray class - N-dimentional array with contravariant and covariant indicies.
+    Classes declared in arraypy.py file.
+    
+    There is also modules:
+    1) tensor_methods.py       - Tensor algebra.
+    2) tensor_fields.py        - operation in tensor fields like diff, rot, lie, etc...
+    3) riemannian_geometry.py  - tensor operations about some riemannian geometry, ok?
+    4) helper_functions.py     - some help functions.
+    """
+)
